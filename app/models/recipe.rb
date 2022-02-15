66ec72dab530b6c66830cf36c008d04def3f7740
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-02-14 21:50:43 -0800

    created connection between user and recipe using join table user_recipe_boxes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
     has_many :reviews
-    has_many :user_recipe_boxs
-    has_many :users, through: :user_recipe_boxs
+    has_many :user_recipe_boxes
+    has_many :users, through: :user_recipe_boxes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :reviews
-    has_many :user_recipe_boxs
-    has_many :recipes, through: :user_recipe_boxs
+    has_many :user_recipe_boxes
+    has_many :recipes, through: :user_recipe_boxes
 end
