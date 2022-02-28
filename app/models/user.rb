62c971b2363ce92006c6dbfd63c1c64e78427f9c
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-02-27 16:00:56 -0800

    updated model to all for password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :user_recipe_boxes
     has_many :recipes, through: :user_recipe_boxes
+    has_secure_password
 end
